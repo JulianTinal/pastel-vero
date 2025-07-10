a5c43884f44d85ccb2d3c8e78fcbfe02897f4133
JulianTinal <julian.tinal.15tvsec@gmail.com>
2025-07-09 19:11:58 -0600

    Force Netlify build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '/public/images/pastel1.jpg',
+        '/public/images/pastel2.jpg',
+        '/public/images/pastel3.jpg',
+        '/public/images/pastel4.jpg',
+        '/public/images/pastel5.jpg',
+        '/public/images/pastel6.jpg',
+        '/public/images/pastel7.jpg',
+      ],
+    },
+  },
 })
